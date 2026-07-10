48f029b80835a268e4d4c8d451ed007985ccf2ba
Davi <davi.rferreira@gmail.com>
2026-07-10 13:37:24 -0300

    03-Comecando-no-Spring-Boot | 01-Fundamentos-do-Spring - Criando a primeira controller e rota no spring

---
+++ 03-Comecando-no-Spring-Boot/01-Fundamentos-do-Spring/src/main/java/br/com/daviferreira/primeiroprojetospring/PrimeiraController.java
@@ -0,0 +1,16 @@
+package br.com.daviferreira.primeiroprojetospring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello-world")
+public class PrimeiraController {
+
+    @GetMapping()
+    public String primeiroMetodo() {
+        return "Hello, World! Esse é meu primeiro métod";
+    }
+
+}
