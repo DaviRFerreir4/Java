b7d25c13da9fb1a6638913f68637f60aa8d04f1c
Davi <davi.rferreira@gmail.com>
2026-07-13 08:43:30 -0300

    03-Comecando-no-Spring-Boot | 01-Fundamentos-do-Spring - Usando route e query params

--- 03-Comecando-no-Spring-Boot/01-Fundamentos-do-Spring/src/main/java/br/com/daviferreira/controller/PrimeiraController.java
+++ 03-Comecando-no-Spring-Boot/01-Fundamentos-do-Spring/src/main/java/br/com/daviferreira/controller/PrimeiraController.java
@@ -1,16 +1,37 @@
 package br.com.daviferreira.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
-@RequestMapping("/hello-world")
+@RequestMapping("/")
 public class PrimeiraController {
 
-    @GetMapping()
+    @GetMapping("/hello-world")
     public String primeiroMetodo() {
         return "Hello, World! Esse é meu primeiro método";
     }
 
+    // método com route params
+    @GetMapping("/users/{id}")
+    public String showUser(@PathVariable(name = "id") String outroNome) {
+        return String.format("Usuário retornado através de route params: %s",
+                outroNome);
+    }
+
+    // método com query params
+    @GetMapping("/users")
+    public String indexUsers(@RequestParam String id) {
+        return String.format("Usuário retornado através de query params: %s",
+                id);
+
+    }
+
+    // método com query params num map
+    @GetMapping("/users-with-filters")
+    public String indexUsersWithFilters(@RequestParam Map<String, String> allParams) {
+        return String.format("Usuário retornado através de query params: %s",
+                allParams.entrySet());
+    }
 }
