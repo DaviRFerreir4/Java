2e36df05d1592dcd83e68d0c4d43b7f135425bd6
Davi <davi.rferreira@gmail.com>
2026-07-13 08:32:16 -0300

    03-Comecando-no-Spring-Boot | 01-Fundamentos-do-Spring - Usando ComponentScan para buscar scanear classes fora do pacote base do projeto

--- 03-Comecando-no-Spring-Boot/01-Fundamentos-do-Spring/src/main/java/br/com/daviferreira/primeiroprojetospring/Application.java
+++ 03-Comecando-no-Spring-Boot/01-Fundamentos-do-Spring/src/main/java/br/com/daviferreira/primeiroprojetospring/Application.java
@@ -2,7 +2,9 @@ package br.com.daviferreira.primeiroprojetospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "br.com.daviferreira")
 @SpringBootApplication
 public class Application {
 
